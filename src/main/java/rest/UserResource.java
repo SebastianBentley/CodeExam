663dae49dee1ca635c70ee7a407087d630932abb
Sebastian <cph-sb287@cphbusiness.dk>
2021-01-18 20:05:23 +0100

    registeruser

--- src/main/java/rest/UserResource.java
+++ src/main/java/rest/UserResource.java
@@ -4,13 +4,18 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import dtos.ActivityDTO;
+import entities.Activity;
 import entities.User;
+import errorhandling.API_Exception;
+import facades.UserFacade;
 import java.io.IOException;
 import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.TypedQuery;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
@@ -19,9 +24,10 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
+import static rest.ActivityResource.ACTIVITY_FACADE;
+import security.errorhandling.AuthenticationException;
 import utils.EMF_Creator;
 import utils.HttpUtils;
-
 
 @Path("member")
 public class UserResource {
@@ -30,6 +36,8 @@ public class UserResource {
     private Gson gson = new Gson();
 
     private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory();
+    public static final UserFacade USER_FACADE = UserFacade.getUserFacade(EMF);
+    
     @Context
     private UriInfo context;
 
@@ -67,8 +75,25 @@ public class UserResource {
         return "{\"msg\": \"Hello to User: " + thisuser + "\"}";
     }
 
-    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("register")
+    public String createActivity(String jsonString) throws API_Exception, AuthenticationException {
 
+        try {
+            JsonObject json = JsonParser.parseString(jsonString).getAsJsonObject();
+            String username = json.get("username").getAsString();
+            String password = json.get("password").getAsString();
+            String age = json.get("age").getAsString();
+            String weight = json.get("weight").getAsString();
 
+            USER_FACADE.registerUser(username, password, age, weight);
+            
+        } catch (Exception e) {
+            return "{\"msg\":\"Something went wrong\"}";
+        }
+        return "{\"msg\":\"User Registered\"}";
+    }
 
 }
